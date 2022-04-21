e1587c63dc9da33d29134c59f9c863b64a6a3894
DarianShawn <DarianShawn007@gmail.com>
2022-04-21 10:55:12 +0800

    Add bridge contract abi

--- contracts/abis/abis.go
+++ contracts/abis/abis.go
@@ -5,7 +5,14 @@ import (
 )
 
 // Predeployed system contract ABI
-var StakingABI = abi.MustNewABI(StakingJSONABI)
+var (
+	// staking contract (0x0000000000000000000000000000000000001001) abi
+	StakingABI = abi.MustNewABI(StakingJSONABI)
+	// bridge contract (0x0000000000000000000000000000000000001002) abi
+	BridgeABI = abi.MustNewABI(BridgeJSONABI)
+	// vault contract (0x0000000000000000000000000000000000001003) abi
+	VaultABI = abi.MustNewABI(VaultJSONABI)
+)
 
 // Temporarily deployed contract ABI
 var StressTestABI = abi.MustNewABI(StressTestJSONABI)
