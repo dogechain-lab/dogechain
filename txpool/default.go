65bb76f5de9af20bf0de7321fb51b64fff77fd11
DarianShawn <DarianShawn007@gmail.com>
2022-09-02 11:09:52 +0800

    default value

--- txpool/default.go
+++ txpool/default.go
@@ -5,6 +5,8 @@ const (
 	DefaultPromoteOutdateSeconds = 3600 // not promoted account for a long time would be pruned
 	// txpool transaction max slots. tx <= 32kB would only take 1 slot. tx > 32kB would take
 	// ceil(tx.size / 32kB) slots.
-	DefaultMaxSlots            = 4096
-	DefaultMaxAccountDemotions = 10 // account demotion counter limit
+	DefaultMaxSlots                = 4096
+	DefaultMaxAccountDemotions     = 10 // account demotion counter limit
+	DefaultClippingMemoryThreshold = 80 // when it reach 80%, it will trigger the clipping
+	DefaultClippingTickSeconds     = 60 // clipping check duration, in seconds.
 )
