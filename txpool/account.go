b206f5c243cc2e2af084de85c2ca4db0db20c601
DarianShawn <DarianShawn007@gmail.com>
2022-09-07 14:20:25 +0800

    reduce accountQueue expose fields

--- txpool/account.go
+++ txpool/account.go
@@ -142,7 +142,7 @@ func (m *accountsMap) allTxs(includeEnqueued bool) (
 		defer account.promoted.unlock()
 
 		if account.promoted.length() != 0 {
-			allPromoted[addr] = account.promoted.queue
+			allPromoted[addr] = account.promoted.Transactions()
 		}
 
 		if includeEnqueued {
@@ -150,7 +150,7 @@ func (m *accountsMap) allTxs(includeEnqueued bool) (
 			defer account.enqueued.unlock()
 
 			if account.enqueued.length() != 0 {
-				allEnqueued[addr] = account.enqueued.queue
+				allEnqueued[addr] = account.enqueued.Transactions()
 			}
 		}
 
