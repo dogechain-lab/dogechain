ce074404bc139f14c6bfe47f66fce7749a1fd1d7
DarianShawn <DarianShawn007@gmail.com>
2022-09-08 17:33:36 +0800

    Remove parallel running to make test pass

--- txpool/txpool_test.go
+++ txpool/txpool_test.go
@@ -2076,8 +2076,6 @@ func TestPopAndRequeue(t *testing.T) {
 
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
-			t.Parallel()
-
 			// helper callback for transition errors
 			status := func(tx *types.Transaction) (s status) {
 				txs := test.allTxs[tx.From]
